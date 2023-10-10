f9c4735ae235055545b2c0694c62db89c552c3e9
Claes Östlund <claes@stenbergit.se>
2023-10-10 12:52:27 +0200

    1.1.0

--- PluginCheckbizApi.php
+++ PluginCheckbizApi.php
@@ -1,12 +1,32 @@
 <?php
 class PluginCheckbizApi{
   public $token = null;
+  private $url = 'https://api.checkbiz.se/api/v1';
   public function get_personalinformation($SSN){
     wfPlugin::includeonce('server/json');
     $server = new PluginServerJson();
-    $url = 'https://api.checkbiz.se/api/v1';
     $get = '/personinformation?SSN='.$SSN;
-    $result = $server->get($url.$get, array('Package' => 'PersonInformation', 'Authorization' => 'Basic '.$this->token));
+    $result = $server->get($this->url.$get, array('Package' => 'PersonInformation', 'Authorization' => 'Basic '.$this->token));
+    /**
+     * db
+     */
+    wfPlugin::includeonce('checkbiz/db');
+    $db = new PluginCheckbizDb();
+    $insert = array();
+    $insert['SSN'] = $SSN;
+    $insert['ssnStatus'] = $result['ssnStatus'];
+    $insert['responseCode'] = $result['responseCode'];
+    $insert['responseMessage'] = $result['responseMessage'];
+    foreach($result['basic'] as $k => $v){
+      $insert["basic_$k"] = $v;
+    }
+    foreach($result['extended'] as $k => $v){
+      $insert["extended_$k"] = $v;
+    }
+    $db->checkbiz_personalinformation_insert($insert);
+    /**
+     * 
+     */
     return $result;
   }
 }
